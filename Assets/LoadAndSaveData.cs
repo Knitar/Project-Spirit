8d3b367d165a6309c22b69c6ea6153f195de030e
Knitar <mael76984@gmail.com>
2024-12-16 18:02:10 +0100

    Skeleton of Save
    Just the structure of the Save mecanics.
    Will change it after some code are Made
    The implementation of Game Manager.

---
+++ Assets/LoadAndSaveData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class LoadAndSaveData : MonoBehaviour
+{
+
+    //public static LoadAndSaveData machin machin;
+    void Start()
+    {
+        
+    }
+
+    public void SaveData()
+    {
+       //PlayerPrefs.SetInt(nom de la variable, le nombre à sauvegarder);
+    }
+
+    
+
+}
