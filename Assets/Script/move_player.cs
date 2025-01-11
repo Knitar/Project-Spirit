49cc630f0f146c9138245cc8f0dc6151fa7fb9a8
STMaTou <mathieu.palomino-gutierrez@epita.fr>
2025-01-11 17:09:17 +0100

    Reapply "Version 2"
    This reverts commit 085c3a9aa3cd07cfe0877adbfaa2fe9983a9b453.

--- Assets/Script/move_player.cs
+++ Assets/Script/move_player.cs
@@ -5,17 +5,40 @@ public class move_player : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero; 
+    public Animator animator;
+
+    public SpriteRenderer spriteRenderer;
     void FixedUpdate()
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
         MovePlayer(horizontalMovement);
 
+        Flip(rb.linearVelocity.x);
+
+        float charactervelocity = Mathf.Abs(rb.linearVelocity.x);
+        animator.SetFloat("speed",charactervelocity);
+
     }
 
+    /***************************** methode pour qu'il bouge *****************/
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y); // rb.Velocity.y
         rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity,targetVelocity, ref velocity, .05f); 
+    }
+
+    /********************************** Retourne le personnage *******************************/
+
+    void Flip(float _velocity)
+    {
+        if(_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
