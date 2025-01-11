23b5542e8f130ec176dc197fac9afe606ba07638
Pepebanjo <antonio.matassa@epita.fr>
2025-01-11 17:12:56 +0100

    Revert "Encore"
    This reverts commit 299acd16fa755706d632396749949c0be65ce9f1, reversing
    changes made to 47685e4b318941c9f3249c4685e1c92fec91eea5.

--- Assets/Script/move_player.cs
+++ Assets/Script/move_player.cs
@@ -4,15 +4,9 @@ public class move_player : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
-<<<<<<< HEAD
     private Vector3 velocity = Vector3.zero;
 
     public Animator animator; 
-=======
-    private Vector3 velocity = Vector3.zero; 
-    public Animator animator;
-
->>>>>>> a935972cacf1dcc8b62410e85150030b1ecaab36
     public SpriteRenderer spriteRenderer;
     void FixedUpdate()
     {
@@ -23,42 +17,23 @@ public class move_player : MonoBehaviour
 
         Flip(rb.linearVelocity.x);
 
-<<<<<<< HEAD
         float CharacterVelocity = Mathf.Abs(rb.linearVelocity.x);
 
         animator.SetFloat("Speed", CharacterVelocity);
-=======
-        float charactervelocity = Mathf.Abs(rb.linearVelocity.x);
-        animator.SetFloat("speed",charactervelocity);
-
->>>>>>> a935972cacf1dcc8b62410e85150030b1ecaab36
     }
 
-    /***************************** methode pour qu'il bouge *****************/
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y); // rb.Velocity.y
         rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity,targetVelocity, ref velocity, .05f); 
     }
 
-<<<<<<< HEAD
     void Flip(float _velocity)
     {
         if (_velocity > 0.1f)
         {
             spriteRenderer.flipX = false;
         } else if(_velocity < -0.1f)
-=======
-    /********************************** Retourne le personnage *******************************/
-
-    void Flip(float _velocity)
-    {
-        if(_velocity > 0.1f)
-        {
-            spriteRenderer.flipX = false;
-        }
-        else if(_velocity < -0.1f)
->>>>>>> a935972cacf1dcc8b62410e85150030b1ecaab36
         {
             spriteRenderer.flipX = true;
         }
