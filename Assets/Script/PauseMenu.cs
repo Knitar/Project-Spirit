dc661cf712928125228a1ebdff9f905e1a14c97c
Knitar <mael76984@gmail.com>
2025-01-14 10:15:53 +0100

    Clean Github

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool pause = false;
+    public GameObject MenupauseUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pause)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    void Paused() 
+    {
+        MenupauseUI.SetActive(true);
+        Time.timeScale = 0;
+        pause = true;
+    }
+
+    public void Resume() 
+    {
+        MenupauseUI.SetActive(false);
+        Time.timeScale = 1;
+        pause = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- Assets/Script/move_player.cs
+++ Assets/Script/move_player.cs
@@ -2,57 +2,40 @@ using UnityEngine;
 
 public class move_player : MonoBehaviour
 {
-    public float moveSpeed; // vitesse du perso 
-    public Rigidbody2D rb; // réference à rigibody 2D (unity)
-    private Vector3 velocity = Vector3.zero; // pour "lisser" le mouv du perso 
-    public Animator animator; // animations
-    public SpriteRenderer spriteRenderer; // unity 
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
 
-    public Transform spawnPoint; // le point de spawn de la fleche
+    public Animator animator; 
+    public SpriteRenderer spriteRenderer;
     void FixedUpdate()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // Cette ligne fait bouger le perso 
-        // par contre pour les touches : edit/project-settings/Input-mangager/horizontal
 
-        MovePlayer(horizontalMovement); //appele à la méthode qui calcule le déplacement 
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
-        Flip(rb.linearVelocity.x); //appelle de la fonction qui retourne 
+        MovePlayer(horizontalMovement);
+
+        Flip(rb.linearVelocity.x);
 
         float CharacterVelocity = Mathf.Abs(rb.linearVelocity.x);
 
-        animator.SetFloat("speed", CharacterVelocity); // met à jour l'animation
-
+        animator.SetFloat("Speed", CharacterVelocity);
     }
 
-    /***************************** methode pour qu'il bouge *****************/
     void MovePlayer(float _horizontalMovement)
     {
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y); // vitesse cible du personnage
-        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity,targetVelocity, ref velocity, .05f); // Le SMoothdamp lisse la transition entre la vitesse actuelle et celle ciblée  
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y); // rb.Velocity.y
+        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity,targetVelocity, ref velocity, .05f); 
     }
-    /********************************** Retourne le personnage(litteralement) *******************************/
 
-    void Flip(float _velocity) // retourne le personnage si il se déplace dans le sens inverse
-    //De plus ça flip aussi les spawm point des flèches pour que les flèches soient dans le sens du personnage
+    void Flip(float _velocity)
     {
-        if(_velocity > 0.1f)
+        if (_velocity > 0.1f)
         {
             spriteRenderer.flipX = false;
-
-             if (spawnPoint != null)
-            {
-                spawnPoint.localPosition = new Vector3(Mathf.Abs(spawnPoint.localPosition.x), spawnPoint.localPosition.y, spawnPoint.localPosition.z);
-            }
-        }
-        else if(_velocity < -0.1f)
+        } else if(_velocity < -0.1f)
         {
             spriteRenderer.flipX = true;
-
-            if (spawnPoint != null)
-            {
-                spawnPoint.localPosition = new Vector3(-Mathf.Abs(spawnPoint.localPosition.x), spawnPoint.localPosition.y, spawnPoint.localPosition.z);
-            }
         }
     }
-
 }
